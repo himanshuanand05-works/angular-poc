b0ccc9bcd8b0496d049b566260e28f3ec4e4e7ec
himanshuanand05-works <himanshuanand05.works@gmail.com>
2025-05-28 15:11:33 +0530

    stratus app

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -9,8 +9,8 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape}>
-        <AcmeLogo />
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
 
@@ -43,7 +43,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className="hidden md:hidden"
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing on mobile version"
           />
         </div>
